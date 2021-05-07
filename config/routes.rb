7cd6f28925dbd739a3bab600ec81770df5e7ed6f
PTHBui <49119453+s3786794@users.noreply.github.com>
2021-05-07 11:15:45 +1000

    Dropdown menu finally works

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/collection_index', to: 'collection#collection_index'
   get '/shopping_cart', to: 'fashion#shopping_cart'
   get '/search', to: 'fashion#search'
+  get '/help&support', to: 'static_pages#help&support'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
