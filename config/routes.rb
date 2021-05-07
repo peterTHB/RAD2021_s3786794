5fbd9f1181acdf6ad84bb867e0b343cc0ab22c32
PTHBui <49119453+s3786794@users.noreply.github.com>
2021-05-07 11:06:29 +1000

    basic buttons for cart and search

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/kids', to: 'collection#kids'
   get '/new_arrivals', to: 'collection#new_arrivals'
   get '/collection_index', to: 'collection#collection_index'
+  get '/shopping_cart', to: 'fashion#shopping_cart'
+  get '/search', to: 'fashion#search'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
