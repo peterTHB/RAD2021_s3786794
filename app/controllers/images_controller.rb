429134abbd54d59a2c5b8fb7233e074d40ae5682
PTHBui <49119453+s3786794@users.noreply.github.com>
2021-05-08 12:15:02 +1000

    Backend changes to carousel images

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,14 @@
+class ImagesController < ApplicationController
+  def image
+    # #Change to hash
+    # @collectionImages = Dir.entries("#{Rails.root}/app/assets/images/collection_test/")
+    # @popularImages = Dir.entries("#{Rails.root}/app/assets/images/popular_test/")
+    #
+    # @collectionImagesFiltered = @collectionImages.select { |image| image.match("\.(jpe?g|png)") }
+    # @popularImagesFiltered = @popularImages.select { |image| image.match("\.(jpe?g|png)") }
+    #
+    # # puts @collectionImagesFiltered
+    # # puts @popularImagesFiltered
+
+  end
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
+    #Change to hash
+    @collectionImages = Dir.entries("#{Rails.root}/app/assets/images/collection_test/")
+    @popularImages = Dir.entries("#{Rails.root}/app/assets/images/popular_test/")
+
+    @collectionImagesFiltered = @collectionImages.select { |image| image.match("\.(jpe?g|png)") }
+    @popularImagesFiltered = @popularImages.select { |image| image.match("\.(jpe?g|png)") }
+
+    @collectionImagesFiltered = @collectionImagesFiltered.map { |url| "collection_test/#{url}" }
+    @popularImagesFiltered = @popularImagesFiltered.map { |url| "popular_test/#{url}" }
+
+    puts @collectionImagesFiltered
+    puts @popularImagesFiltered
+  end
+
+  def help_support
 
   end
 end
