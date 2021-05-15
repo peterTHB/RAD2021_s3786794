582320f42afb4ae0a9658c7020974c016cae4d68
PTHBui <49119453+s3786794@users.noreply.github.com>
2021-05-16 01:26:51 +1000

    slightly changed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -22,13 +22,9 @@ Rails.application.routes.draw do
   get '/search', to: 'fashion#search'
   get '/saved_list', to: 'fashion#saved_list'
   get '/help&support', to: 'static_pages#help&support'
-  get 'static_pages/savedToList', to: 'static_pages#savedToList'
   post 'static_pages/savedToList', to: 'static_pages#savedToList'
-  get 'fashion/savedToListOrNot', to: 'fashion#savedToListOrNot'
   post 'fashion/savedToListOrNot', to: 'fashion#savedToListOrNot'
-  get 'fashion/moveForward', to: 'fashion#moveForward'
   post 'fashion/moveForward', to: 'fashion#moveForward'
-  get 'fashion/moveBackward', to: 'fashion#moveBackward'
   post 'fashion/moveBackward', to: 'fashion#moveBackward'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
